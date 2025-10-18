05bdf519e0cc94bc2e2b93390bf396d63f17881b
Mark Salita <markmoenlsalita@gmail.com>
2025-10-18 22:19:17 +0800

    chore: remove local build artifacts from working tree

--- QCGO/obj/Debug/net8.0/QCGO.AssemblyInfo.cs
+++ QCGO/obj/Debug/net8.0/QCGO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QCGO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3739b7f695adfb98c0913617bbcdd27a6dbb96c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44b6f85c67a5c49923b066c968f8e0dc29c56df7")]
 [assembly: System.Reflection.AssemblyProductAttribute("QCGO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QCGO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
